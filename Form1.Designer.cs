072656c63cbb6eb22a27d0429b0793490a2e660e
Zakhar Vashchenko <69220793+urs-zoh@users.noreply.github.com>
2024-07-08 16:14:59 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MyChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.firstChat = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(466, 12);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(322, 284);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMessage_KeyPress);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(563, 340);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(158, 43);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // firstChat
+            // 
+            this.firstChat.FormattingEnabled = true;
+            this.firstChat.ItemHeight = 25;
+            this.firstChat.Location = new System.Drawing.Point(12, 12);
+            this.firstChat.Name = "firstChat";
+            this.firstChat.Size = new System.Drawing.Size(448, 429);
+            this.firstChat.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.firstChat);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "MyChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox firstChat;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtMessage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                SendMessage();
+            }
+        }
+
+        private void SendMessage()
+        {
+            string message = txtMessage.Text.Trim();
+            if (!string.IsNullOrEmpty(message))
+            {
+                firstChat.Items.Add("Me: " + message);
+                txtMessage.Clear();
+                txtMessage.Focus();
+            }
+        }
+    }
+}
